b74dc7d93370007ff39933611e306615c4402589
제현 <jehyeon@seoyeonui-MacBookAir.local>
2025-06-18 00:48:30 +0900

    Fix button display issues - increase bottom padding and enhance button visibility

--- _next/static/CWf5f6yW2rDA0wOsDvmwf/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/776-28be26c730445468.js","static/chunks/pages/index-b83c8ac28ee22efd.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
